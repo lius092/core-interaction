e5185d87ff4bebc1c4cf52e9110477e64d6f7c27
lius092 <31445064+lius092@users.noreply.github.com>
2018-05-03 10:42:08 -0400

    temp change

--- studio/Homepage/main.js
+++ studio/Homepage/main.js
@@ -6,7 +6,7 @@ states.forEach(function(state) {
   state.addEventListener('mouseover', function() {
     var zip = state.dataset.zip
 
-    fetch('https://api.openweathermap.org/data/2.5/weather?zip=' + zip + '&APPID=3ff5ccf77982ee84f36e9c8253113ba2&units=imperial')
+    fetch('http://api.openweathermap.org/data/2.5/weather?zip=' + zip + '&APPID=3ff5ccf77982ee84f36e9c8253113ba2&units=imperial')
       .then(function(response) {
         // Get the response and format it to JSON
         return response.json();
