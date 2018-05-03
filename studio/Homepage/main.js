9cad23e163e812f5f3880c2227e15d2ea123dc0f
lius092 <31445064+lius092@users.noreply.github.com>
2018-05-03 11:32:43 -0400

    apitemp

--- studio/Homepage/main.js
+++ studio/Homepage/main.js
@@ -1,10 +1,13 @@
 var states = Array.from(document.querySelectorAll('.state'));
 // var locationSpan = document.querySelector('.temp-location');
 var degSpan = document.querySelector('.temp-deg');
+var tempDiv = document.querySelector('.temp');
+
 
 states.forEach(function(state) {
   state.addEventListener('mouseover', function() {
     var zip = state.dataset.zip
+    tempDiv.style.opacity=1
 
     fetch('https://api.openweathermap.org/data/2.5/weather?zip=' + zip + '&APPID=3ff5ccf77982ee84f36e9c8253113ba2&units=imperial')
       .then(function(response) {
@@ -21,4 +24,7 @@ states.forEach(function(state) {
       });
 
   });
+    state.addEventListener('mouseout', function() {
+      tempDiv.style.opacity=0
+    });
 });
